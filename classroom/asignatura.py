01d70a17c0511c0e4bd60260ae9c576582eda7ab
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2024-02-29 21:41:17 -0500

    Intento #2

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,19 +1,10 @@
-
-import subprocess
-subprocess.run(["pip", "install","multimethod"])
-from multimethod import multimethod
-
 class Asignatura:
 
-    @multimethod
+
     def __init__(self, nombre, salon = "remoto"):
         self._nombre = nombre
         self._salon = salon
     
-    @multimethod
-    def __init__(self, nombre, salon = "remoto"):
-        self._nombre = nombre
-        self._salon = salon
 
     def __str__(self):
         return f"{self._nombre} {self._salon}"
