63c3b284edbb86b475c82dfde67cb4aeefb426e5
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2024-02-29 21:21:21 -0500

    OK

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,17 @@
+from multimethod import multimethod
+
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    @multimethod
+    def __init__(self, nombre, salon, tipo = "remoto"):
         self._nombre = nombre
         self._salon = salon
+        self._tipo = tipo
+    
+    @multimethod
+    def __init__(self, nombre, tipo = "remoto"):
+        self._nombre = nombre
+        self._tipo = tipo
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"{self._nombre} {self._tipo}"
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,35 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo : str = "grupo ordinado", asignaturas : list = [], estudiantes : list =[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=[]):
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
+        lista.clear()
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        msg = "Grupo de estudiantes: "
+        return msg + self._grupo if self._grupo != "grupo ordinado" else msg +"grupo predeterminado"
 
-    @ classmethod
+    """@ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+        cls.grado = nombre"""
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
+    """@ classmethod
     def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+        cls.grado = nombre"""
